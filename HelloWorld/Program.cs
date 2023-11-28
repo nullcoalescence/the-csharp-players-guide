df3f191e58138286b2e4d534616da2ffcbaf0bd0
ben <{username}@users.noreply.github.com>
2023-11-27 21:06:51 -0600

    more challenges

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // level 3 challenge
+            Console.WriteLine("Something else...");
+
+            // Challenge
+            Console.WriteLine("One more Console.WriteLine()");
+            Console.WriteLine("Another Console.WriteLine()");
+            Console.WriteLine("Yet another Console.WriteLine()");
+            Console.WriteLine("Uno mas Console.WriteLine()");
+            Console.WriteLine("Last but not least Console.WriteLine()");
         }
     }
 }
